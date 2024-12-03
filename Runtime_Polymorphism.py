2a623eaa958a4295106787c835335ad31e0a2a1f
Akash_Raut <136334052+Akash-Raut2000@users.noreply.github.com>
2024-12-03 20:47:31 +0530

    Add files via upload

---
+++ Runtime_Polymorphism.py
@@ -0,0 +1,16 @@
+class Animal:
+    def sound(self):
+        return "Some generic sound"
+
+class Dog(Animal):
+    def sound(self):
+        return "Bark"
+
+class Cat(Animal):
+    def sound(self):
+        return "Meow"
+
+# Runtime polymorphism
+animals = [Animal(), Dog(), Cat()]
+for animal in animals:
+    print(animal.sound())
